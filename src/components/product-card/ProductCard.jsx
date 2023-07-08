74e8441ae363dbb0ea58768d63052777c9b888c4
erginadem <erginadem@gmail.com>
2023-07-07 23:51:53 -0600

    v2- fixed some bugs

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -23,13 +23,13 @@ const ProductCard = (props) => {
       <Card.Img variant="top" src={`assets/products/${image}`} />
       <Card.Body className="text-center">
         {
-          (!discount == 0) && <div className="top-discount">%{discount}</div>
+          (!discount === 0) && <div className="top-discount">%{discount}</div>
         }
         <h3 className="text-warning fs-4">{RatingStars({ rate })}</h3>
         <h4 className="text-uppercase fs-4">{title}</h4>
         <Card.Text>
           {
-            (discount == 0) ? (<Card.Text>
+            (discount === 0) ? (<Card.Text>
               <span className="text-success fs-4">${price}</span>
             </Card.Text>) : (<Card.Text>
               <span className="me-2 text-secondary fs-4 text-decoration-line-through me-3">${price}</span>
